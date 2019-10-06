e33d7d11cd54265d5945edb90a00171d91b73532
5940 User <5940user@DMC-3s-MacBook-Pro.local>
2019-10-06 15:04:30 -0700

    create left and right motors

--- src/main/kotlin/frc/robot/Controls.kt
+++ src/main/kotlin/frc/robot/Controls.kt
@@ -1,5 +1,6 @@
 package frc.robot
 
+import frc.robot.subsystems.drive.DriveCommand
 import org.ghrobotics.lib.wrappers.hid.* // ktlint-disable no-wildcard-imports
 
 object Controls {
@@ -7,6 +8,8 @@ object Controls {
     // This is the xbox controller on port 0 of the driverstation
     val driverFalconXbox = xboxController(0) {
         registerEmergencyMode()
+        button(kB).change(DriveCommand())
+
     }
 
     fun update() {
--- src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
@@ -1,13 +1,20 @@
 package frc.robot.subsystems.drive
 
+import edu.wpi.first.wpilibj.VictorSP
 import org.ghrobotics.lib.commands.FalconCommand
 
 class DriveCommand : FalconCommand(DriveSubsystem) {
     override fun isFinished() = false
 
     override fun initialize() {
+        DriveSubsystem.leftMotor.set(0.8)
+        DriveSubsystem.rightMotor.set(0.8)
     }
 
     override fun end(interrupted: Boolean) {
+        DriveSubsystem.leftMotor.stopMotor()
+        DriveSubsystem.rightMotor.stopMotor()
     }
+
+
 }
