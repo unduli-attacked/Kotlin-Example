5cd68f4c40c58cdc53e1d2f701e082220a2eaf5d
dmc-account <55603701+dmc-account@users.noreply.github.com>
2019-10-06 15:59:45 -0700

    Right motor movement

--- src/main/kotlin/frc/robot/Controls.kt
+++ src/main/kotlin/frc/robot/Controls.kt
@@ -8,7 +8,7 @@ object Controls {
     // This is the xbox controller on port 0 of the driverstation
     val driverFalconXbox = xboxController(0) {
         registerEmergencyMode()
-        button(kB).change(DriveCommand())
+        button(1).change(DriveCommand())
     }
 
     fun update() {
--- src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
@@ -6,10 +6,10 @@ class DriveCommand : FalconCommand(DriveSubsystem) {
     override fun isFinished() = false
 
     override fun initialize() {
-        DriveSubsystem.leftMotor.set(0.5)
+        DriveSubsystem.rightMotor.set(0.5)
     }
 
     override fun end(interrupted: Boolean) {
-        DriveSubsystem.leftMotor.set(0.0)
+        DriveSubsystem.rightMotor.set(0.0)
     }
 }
