d9719358744b5ea073111f76a0bc73766557dd67
dmc-account <55603701+dmc-account@users.noreply.github.com>
2019-10-06 16:43:37 -0700

    Adding controls

--- src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
@@ -1,15 +1,39 @@
 package frc.robot.subsystems.drive
 
+import edu.wpi.first.wpilibj.GenericHID
+import frc.robot.Controls
 import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.utils.withDeadband
+import org.ghrobotics.lib.wrappers.hid.getX
+import org.ghrobotics.lib.wrappers.hid.getY
 
 class DriveCommand : FalconCommand(DriveSubsystem) {
+    override fun initialize() {
+    }
+
     override fun isFinished() = false
 
-    override fun initialize() {
-        DriveSubsystem.rightMotor.set(0.5)
+    override fun execute() {
+        // negative because xbox is negative Y when pushed forward
+        val forward = -speedSource() // same as -1 * speedSource.invoke()
+        val turn = rotationSource()
+
+        val wantedLeftOutput = forward
+        val wantedRightOutput = forward
+
+        DriveSubsystem.leftMotor.setSpeed(wantedLeftOutput)
+        DriveSubsystem.rightMotor.setSpeed(wantedRightOutput)
     }
 
     override fun end(interrupted: Boolean) {
-        DriveSubsystem.rightMotor.set(0.0)
+        DriveSubsystem.leftMotor.setSpeed(0.0)
+        DriveSubsystem.rightMotor.setSpeed(0.0)
     }
+
+    companion object {
+        private const val kDeadband = 0.05
+        val speedSource by lazy { Controls.driverFalconXbox.getY(GenericHID.Hand.kLeft).withDeadband(kDeadband) }
+        private val rotationSource by lazy { Controls.driverFalconXbox.getX(GenericHID.Hand.kRight) }
+    }
+
 }
