8a6da66368d7a8e2ea115b6a6d11bdd28dfafc44
afurlan7 <55670341+afurlan7@users.noreply.github.com>
2019-10-08 10:21:15 -0700

    Using the joystick to drive

--- src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveCommand.kt
@@ -1,20 +1,54 @@
 package frc.robot.subsystems.drive
 
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.VictorSP
+import frc.robot.Controls
 import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.utils.withDeadband
+import org.ghrobotics.lib.wrappers.hid.getX
+import org.ghrobotics.lib.wrappers.hid.getY
 
 class DriveCommand : FalconCommand(DriveSubsystem) {
     override fun isFinished() = false
 
     override fun initialize() {
-        DriveSubsystem.leftMotor.set(0.8)
-        DriveSubsystem.rightMotor.set(0.8)
+        DriveSubsystem.leftMotor.setDutyCycle(0.8)
+        DriveSubsystem.rightMotor.setDutyCycle(0.8)
     }
 
     override fun end(interrupted: Boolean) {
-        DriveSubsystem.leftMotor.stopMotor()
-        DriveSubsystem.rightMotor.stopMotor()
+        DriveSubsystem.leftMotor.setDutyCycle(0.0)
+        DriveSubsystem.rightMotor.setDutyCycle(0.0)
     }
 
+class TeleopDriveCommand : FalconCommand(DriveSubsystem){
 
+    override fun isFinished() = false
+
+    override fun execute() {
+        //negative because Xbox is negative Y when pushed forward
+        val forward = -speedSource()  // same as -1 * speedSource.invoke()
+        val turn = rotationSource()
+
+        val wantedLeftOutput = forward + turn
+        val wantedRightOutput = forward - turn
+
+        DriveSubsystem.leftMotor.setDutyCycle(wantedLeftOutput)
+        DriveSubsystem.rightMotor.setDutyCycle(wantedRightOutput)
+
+
+
+    }
+    override fun end (interrupted: Boolean){
+        DriveSubsystem.leftMotor.setNeutral()
+        DriveSubsystem.rightMotor.setNeutral()
+
+    }
+
+    companion object {
+        private const val kDeadband = 0.05
+        val speedSource by lazy{Controls.driverFalconXbox.getY(GenericHID.Hand.kLeft).withDeadband(kDeadband) }
+        val rotationSource by lazy{Controls.driverFalconXbox.getX(GenericHID.Hand.kRight) }
+    }
+}
 }
